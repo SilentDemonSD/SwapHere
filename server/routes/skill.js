eb45e38d1d0cece6bd30fd7ed1f754fb14601849
ritikteotia <ritikteotiaone4@gmail.com>
2025-04-11 22:03:58 +0530

    feat: Add V2 Next.JS mode

--- server/routes/skill.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const skillController = require('../controllers/skillController');
-const auth = require('../middleware/auth');
-
-router.get('/', skillController.getSkills);
-router.post('/', auth, skillController.addSkill);
-
-module.exports = router;
--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const auth = require('../middleware/auth');
-
-router.get('/me', auth, userController.getMe);
-
-module.exports = router;
