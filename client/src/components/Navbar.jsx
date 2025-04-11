9506266524af3b7c231babf835fd4c469b597881
SilentDemonSD <drxxstrange@gmail.com>
2025-04-11 03:26:03 -0300

    fix: check

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import { Menu as MenuIcon } from '@mui/icons-material';
 
 const Navbar = () => (
-  <AppBar position="static" sx={{ background: 'rgba(255, 255, 255, 0.1)', backdropFilter: 'blur(10px)', padding: '10px 20px', boxShadow: '0 4px 10px rgba(0, 0, 0, 0.3)'}}>
+  <AppBar position="static" sx={{ background: 'rgba(255, 255, 255, 0.1)', backdropFilter: 'blur(10px)' }}>
     <Toolbar>
       <IconButton edge="start" color="inherit" aria-label="menu">
         <MenuIcon />
